94b80573d9055a6baebb71851c2b290837e228ef
oguzaktas <oguzaktas0x@gmail.com>
2020-05-14 01:23:41 +0300

    Updates

---
+++ precache-manifest.1025bfbe6fe867f58215203aac6d838c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02120322be6b6744313927d298ba8e40",
+    "url": "/stock-market-tracker/index.html"
+  },
+  {
+    "revision": "5defa17f7a598f6f7ea7",
+    "url": "/stock-market-tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fd38aa1378d631b67d11",
+    "url": "/stock-market-tracker/static/css/main.5f597f01.chunk.css"
+  },
+  {
+    "revision": "5defa17f7a598f6f7ea7",
+    "url": "/stock-market-tracker/static/js/2.9e781c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-market-tracker/static/js/2.9e781c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd38aa1378d631b67d11",
+    "url": "/stock-market-tracker/static/js/main.c8c8cc27.chunk.js"
+  },
+  {
+    "revision": "fa88b88c594e9e1b3dbb",
+    "url": "/stock-market-tracker/static/js/runtime-main.289f1f8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock-market-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/iex.js
+++
@@ -1,5 +0,0 @@
-export const iex = {
-    // api_token: 'pk_12ff756a55f3487285968715b4e7e7d1',
-    api_token: 'pk_5257610957ea48a78b4ad56e6f46b3fb',
-    base_url: 'https://cloud.iexapis.com/stable'
-};
